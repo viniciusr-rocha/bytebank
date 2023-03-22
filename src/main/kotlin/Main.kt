d37c5fa7f6309e8f15adacb111f696373a77774d
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-22 17:34:52 -0300

    feat: using object expression

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,24 @@
 import br.com.vinicius.bytebank.basic_of_language.testAccounts
 import br.com.vinicius.bytebank.model.account.SavingsAccount
 import br.com.vinicius.bytebank.model.account.accountsTotal
+import br.com.vinicius.bytebank.model.authentication.Authenticated
+import br.com.vinicius.bytebank.model.authentication.InternalSystem
 import br.com.vinicius.bytebank.model.customer.Customer
 
 fun main() {
+
+    val fran = object : Authenticated {
+        val name: String = "Fran"
+        val documentNumber: String = ""
+        val password: Int = 100
+
+        override fun authenticate(password: Int) = this.password == password
+    }
+
+    val internalSystem = InternalSystem()
+    internalSystem.authenticate(fran, 100)
+
+    println("Nome do cliente ${fran.name}")
 
     val kotlin = Customer("kotlin", "", 1)
     val savingsAccount = SavingsAccount(kotlin, 2323)
