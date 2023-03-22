1d0fc93a74e039441606e6dfdb0503b9616b6cb6
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-22 18:45:57 -0300

    feat: overriding equals and hashcode

--- src/main/kotlin/br/com/vinicius/bytebank/model/customer/Address.kt
+++ src/main/kotlin/br/com/vinicius/bytebank/model/customer/Address.kt
@@ -18,4 +18,31 @@ class Address(
             complement='$complement')
             """.trimIndent()
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Address
+
+        if (address != other.address) return false
+        if (number != other.number) return false
+        if (neighborhood != other.neighborhood) return false
+        if (city != other.city) return false
+        if (zipcode != other.zipcode) return false
+        if (complement != other.complement) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = address.hashCode()
+        result = 31 * result + number
+        result = 31 * result + neighborhood.hashCode()
+        result = 31 * result + city.hashCode()
+        result = 31 * result + zipcode.hashCode()
+        result = 31 * result + complement.hashCode()
+        return result
+    }
+
 }
