71ee3259d9d90947c95295cf7010f4261b3d3a2b
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-22 16:48:10 -0300

    feat: implemented global variable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
+import br.com.vinicius.bytebank.basic_of_language.testAccounts
+import br.com.vinicius.bytebank.model.account.SavingsAccount
+import br.com.vinicius.bytebank.model.account.accountsTotal
+import br.com.vinicius.bytebank.model.customer.Customer
+
 fun main() {
+
+    val kotlin = Customer("kotlin", "", 1)
+    val savingsAccount = SavingsAccount(kotlin, 2323)
+    testAccounts()
+    println("Total de contas $accountsTotal")
 
 }
--- src/main/kotlin/br/com/vinicius/bytebank/model/account/Account.kt
+++ src/main/kotlin/br/com/vinicius/bytebank/model/account/Account.kt
@@ -2,12 +2,20 @@ package br.com.vinicius.bytebank.model.account
 
 import br.com.vinicius.bytebank.model.customer.Customer
 
+var accountsTotal = 0
+    private set
+
 abstract class Account(
     var customer: Customer,
     val number: Int,
 ) {
     var balanceAvailable = 0.0
         protected set
+
+    init {
+        println("Criando conta")
+        accountsTotal++
+    }
 
     fun deposit(amount: Double) {
         this.balanceAvailable += amount
