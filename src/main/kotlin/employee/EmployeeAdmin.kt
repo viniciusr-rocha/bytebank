74c72521567614f1008f115af2883c5753224e1e
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-22 13:37:37 -0300

    feat: inheriting more than one class

--- src/main/kotlin/employee/EmployeeAdmin.kt
+++ src/main/kotlin/employee/EmployeeAdmin.kt
@@ -1,12 +1,15 @@
 package employee
 
 abstract class EmployeeAdmin(
-    val name: String,
-    val documentNumber: String,
-    val remuneration: Double,
-    val password: Int
+    name: String,
+    documentNumber: String,
+    remuneration: Double,
+    val password: Int,
+) : Employee(
+    name,
+    documentNumber,
+    remuneration
 ) {
-    abstract val bonus: Double
 
     fun authentication(password: Int): Boolean {
         if (this.password == password) {
