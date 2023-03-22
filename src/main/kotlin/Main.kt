620144e0c538ec86ee8a3cdbe9d0eafbaf1ad52e
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-22 18:29:29 -0300

    feat: using any

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,16 @@
-import br.com.vinicius.bytebank.basic_of_language.testAccounts
-import br.com.vinicius.bytebank.model.account.Account
-import br.com.vinicius.bytebank.model.account.SavingsAccount
-import br.com.vinicius.bytebank.model.authentication.Authenticated
-import br.com.vinicius.bytebank.model.authentication.InternalSystem
-import br.com.vinicius.bytebank.model.customer.Customer
+import br.com.vinicius.bytebank.model.customer.Address
 
 fun main() {
+    val address = Address()
 
-    val fran = object : Authenticated {
-        val name: String = "Fran"
-        val documentNumber: String = ""
-        val password: Int = 100
+    print(Any())
 
-        override fun authenticate(password: Int) = this.password == password
-    }
+    print(1)
+    print(1.0)
+    print(address)
+}
 
-    val internalSystem = InternalSystem()
-    internalSystem.authenticate(fran, 100)
-
-    println("Nome do cliente ${fran.name}")
-
-    val kotlin = Customer("kotlin", "", 1)
-    val savingsAccount = SavingsAccount(kotlin, 2323)
-    testAccounts()
-    println("Total de contas ${Account.total}")
-
+fun print(value: Any): Any {
+    println(value)
+    return value
 }
---
+++ src/main/kotlin/br/com/vinicius/bytebank/basic_of_language/testObjectsExpressionAndDeclarationAndCompanion.kt
@@ -0,0 +1,27 @@
+package br.com.vinicius.bytebank.basic_of_language
+
+import br.com.vinicius.bytebank.model.account.Account
+import br.com.vinicius.bytebank.model.account.SavingsAccount
+import br.com.vinicius.bytebank.model.authentication.Authenticated
+import br.com.vinicius.bytebank.model.authentication.InternalSystem
+import br.com.vinicius.bytebank.model.customer.Customer
+
+fun testObjectsExpressionAndDeclarationAndCompanion() {
+    val fran = object : Authenticated {
+        val name: String = "Fran"
+        val documentNumber: String = ""
+        val password: Int = 100
+
+        override fun authenticate(password: Int) = this.password == password
+    }
+
+    val internalSystem = InternalSystem()
+    internalSystem.authenticate(fran, 100)
+
+    println("Nome do cliente ${fran.name}")
+
+    val kotlin = Customer("kotlin", "", 1)
+    val savingsAccount = SavingsAccount(kotlin, 2323)
+    testAccounts()
+    println("Total de contas ${Account.total}")
+}
