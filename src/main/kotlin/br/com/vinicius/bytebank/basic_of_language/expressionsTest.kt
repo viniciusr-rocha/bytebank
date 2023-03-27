3d99b1053e70724349a748487846c0cf48ab29a9
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-27 18:11:49 -0300

    feat: throwing exceptions

---
+++ src/main/kotlin/br/com/vinicius/bytebank/basic_of_language/expressionsTest.kt
@@ -0,0 +1,50 @@
+package br.com.vinicius.bytebank.basic_of_language
+
+import br.com.vinicius.bytebank.model.customer.Address
+
+fun expressionsTest() {
+    println("início main")
+
+    val input = "5"
+    val receiveAmount: Double? = try {
+        input.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Probelma na conversao")
+        e.printStackTrace()
+        null
+    }
+
+    val amountTax: Double? = when {
+        receiveAmount != null -> receiveAmount + 0.1
+        else -> {
+            null
+        }
+    }
+
+    println("Valor com taxa: $amountTax")
+
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1() {
+    println("início funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+
+    try {
+        for (i in 1..5) {
+            println(i)
+            val address = Any()
+            address as Address
+        }
+    } catch (e: ClassCastException) {
+//        e.printStackTrace()
+        println("ClassCastException foi pegada")
+    }
+    println("fim funcao2")
+}
