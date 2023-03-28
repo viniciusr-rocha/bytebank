f73a43b7fa3286797ae07a840f3d53e94d437088
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-28 11:05:23 -0300

    feat: using ranges

--- src/main/kotlin/br/com/vinicius/bytebank/collections/MainCollection.kt
+++ src/main/kotlin/br/com/vinicius/bytebank/collections/MainCollection.kt
@@ -1,18 +1,35 @@
 package br.com.vinicius.bytebank.collections
 
 fun main() {
-    val salarios: DoubleArray = doubleArrayOf(1500.0, 2300.0, 5000.0, 8000.0, 10000.0)
-    val aumento = 1.1
-
-    for (indice in salarios.indices) {
-        salarios[indice] = salarios[indice] * aumento
+    val serie: IntRange = 1.rangeTo(10)
+    for (s in serie) {
+        print("$s ")
     }
 
-    println(salarios.contentToString())
+    println()
 
-    salarios.forEachIndexed { i, salario ->
-        salarios[i] = salario * aumento
+    val numerosPares = 2.until(100) step 2
+    for (s in numerosPares) {
+        print("$s ")
     }
 
-    println(salarios.contentToString())
+    println()
+
+    val numerosParesReverso = 100 downTo 0 step 2
+    numerosParesReverso.forEach { print("$it ") }
+
+    println()
+
+    val intervalo = 1500.0..5000.0
+    val salario = 4000.0
+    if (salario in intervalo) {
+        println("Esta dentro do intervalo")
+    } else {
+        println("Nao esta dentro do intervalo")
+    }
+
+    val alfabeto = 'a'..'z'
+    val letra = 'k'
+    println(letra in alfabeto)
+
 }
