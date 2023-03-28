fae941b74accfd5fd28f43d29fb94ebf584ada01
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-28 12:17:54 -0300

    feat: using fold

---
+++ src/main/kotlin/br/com/vinicius/bytebank/basic_of_language/collections/testFold.kt
@@ -0,0 +1,31 @@
+package br.com.vinicius.bytebank.basic_of_language.collections
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+fun testFoldArray() {
+    val salarios: Array<BigDecimal> = bigDecimalArrayOf("1500.55", "2000.00", "5000.00", "10000.00")
+    println("Salarios: ${salarios.contentToString()}")
+
+    val aumento = "1.1".toBigDecimal()
+    val salariosComAumento: Array<BigDecimal> = salarios
+        .map { salario -> calculaAumentoRelativo(salario, aumento) }
+        .toTypedArray()
+
+    println("Salarios com aumento: ${salariosComAumento.contentToString()}")
+
+    val gastoInicial = salariosComAumento.somatoria()
+    println("Gasto incial: $gastoInicial")
+
+    val meses = 6.toBigDecimal()
+    val gastoTotal = salariosComAumento.fold(gastoInicial) { acumulador, salario ->
+        acumulador + (salario * meses).setScale(2, RoundingMode.UP)
+    }
+    println("Gasto total em meses: $gastoTotal")
+}
+
+private fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal): BigDecimal = if (salario < "5000".toBigDecimal()) {
+    salario + "500".toBigDecimal()
+} else {
+    (salario * aumento).setScale(2, RoundingMode.UP)
+}
--- src/main/kotlin/br/com/vinicius/bytebank/collections/MainCollection.kt
+++ src/main/kotlin/br/com/vinicius/bytebank/collections/MainCollection.kt
@@ -1,7 +1,7 @@
 package br.com.vinicius.bytebank.collections
 
-import br.com.vinicius.bytebank.basic_of_language.collections.testObjectsArray
+import br.com.vinicius.bytebank.basic_of_language.collections.testFoldArray
 
 fun main() {
-    testObjectsArray()
+    testFoldArray()
 }
