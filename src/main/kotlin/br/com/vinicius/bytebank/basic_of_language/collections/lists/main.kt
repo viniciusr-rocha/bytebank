d8369b532d7f7ed5cf76790600c2f98be03b6d5e
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-28 17:07:58 -0300

    feat: set behaviors

--- src/main/kotlin/br/com/vinicius/bytebank/basic_of_language/collections/lists/main.kt
+++ src/main/kotlin/br/com/vinicius/bytebank/basic_of_language/collections/lists/main.kt
@@ -4,5 +4,13 @@ fun main() {
     val assistiramCursoAndoid = setOf("Alex", "Fran", "Maria", "Gui")
     val assistiramCursoKotlin = setOf("Alex", "Bia", "Maria", "Jao")
     val assistiramAmbos = assistiramCursoKotlin + assistiramCursoKotlin
-    println(assistiramAmbos)
+
+    println(assistiramCursoKotlin + assistiramCursoAndoid)
+    println(assistiramCursoKotlin union assistiramCursoAndoid)
+
+    println(assistiramCursoKotlin - assistiramCursoAndoid)
+    println(assistiramCursoKotlin subtract assistiramCursoAndoid)
+
+    println(assistiramCursoKotlin intersect assistiramCursoAndoid)
+
 }
