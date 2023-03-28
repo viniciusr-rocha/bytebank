9d1f0b697447d96f34ff7e17c52860a4698c2e70
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-28 01:10:57 -0300

    feat: types that can take null

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,7 @@
-import br.com.vinicius.bytebank.exceptions.InsufficientAmountException
+import br.com.vinicius.bytebank.model.customer.Address
 
 fun main() {
-    funcao1()
-}
-
-fun funcao1() {
-    println("início funcao1")
-    try {
-        funcao2()
-    } catch (e: InsufficientAmountException) {
-        e.printStackTrace()
-        println("ClassCastException foi pegada")
-    }
-    println("fim funcao1")
-}
-
-fun funcao2() {
-    println("início funcao2")
-    for (i in 1..5) {
-        println(i)
-        val address = Any()
-        throw InsufficientAmountException()
-    }
-    println("fim funcao2")
+    var address: Address? = null
+    val addressNotNull: Address = address!!
+    addressNotNull.city
 }
