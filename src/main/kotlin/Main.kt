0831e85d9180f1bd8493dea717c60e96a6ad686b
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-03-28 01:19:49 -0300

    feat: using safe call and let

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 import br.com.vinicius.bytebank.model.customer.Address
 
 fun main() {
-    var address: Address? = null
-    val addressNotNull: Address = address!!
-    addressNotNull.city
+    var address: Address? = Address(city = "Brasilia")
+    println(address?.city?.length?.toString())
+    address?.let {
+        println(it.city.length.toString())
+    }
 }
